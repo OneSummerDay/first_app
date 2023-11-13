71aa5037c3a83352ab65f568dc098404e4556360
Artem Kraievyi <artem.kraievyi@gmail.com>
2023-11-13 20:01:07 +0200

    feat(maim.dart): created "answerTheQuestion" function

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerTheQuestion() {
+    print('Anwer choosen');
+  }
+
   @override
   Widget build(BuildContext context) {
-    var question = ['What is your favorite color?',
-     'What is your favorite animal?'
-     ];
+    var question = [
+      'What is your favorite color?',
+      'What is your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first App'),
         ),
-        body: Column(children: <Widget>[
-          Text('The questin!'),
-          ElevatedButton(onPressed: null, child: Text('Answer 1'),),
-          ElevatedButton(onPressed: null, child: Text('Answer 2'),),
-          ElevatedButton(onPressed: null, child: Text('Answer 3'),)
-        ],),
+        body: Column(
+          children: <Widget>[
+            Text('The questin!'),
+            ElevatedButton(
+              onPressed: answerTheQuestion,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: answerTheQuestion,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: answerTheQuestion,
+              child: Text('Answer 3'),
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
