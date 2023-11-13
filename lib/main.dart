e68fc45629374419080524c8c8bc13b27d5f8038
Artem Kraievyi <artem.kraievyi@gmail.com>
2023-11-13 19:26:19 +0200

    styled: added bar styles and body text

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text('My first App'),),
+      body: Text('My body Text'),
+    ),);
   }
 }
  
